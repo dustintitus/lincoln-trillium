1440e4e282d3192ab709eb651b2e65292961ab07
Anton Zuev <zuev13@mail.ru>
2018-04-17 16:49:30 +0300

    Added "Email Sales" form and button

--- modules/custom/trilliumlincoln_utility/src/Controller/LincolnController.php
+++ modules/custom/trilliumlincoln_utility/src/Controller/LincolnController.php
@@ -96,4 +96,69 @@ class LincolnController extends ControllerBase {
     return $response;
   }
 
+      /**
+   * Callback for opening the modal form Email Sales.
+   */
+  public function openModalEmailSalesForm() {
+    $response = new AjaxResponse();
+
+    // Get the modal form using the form builder.
+    $webform = \Drupal::entityTypeManager()->getStorage('webform')->load('email_sales');
+    $modal_form = $webform->getSubmissionForm();
+
+    $pid = \Drupal::request()->query->get('pid');
+    $product = Product::load($pid);
+
+    if (!empty($modal_form['elements']['sent_from'])) {
+      $url_product = $product->toUrl('canonical', ['absolute' => true]);
+      $modal_form['elements']['sent_from']['#value'] = $url_product->toString();
+    }
+
+    //sku
+    $variations_field = $product->get('variations')->getValue();
+    if (!empty($variations_field)) {
+      $variation = ProductVariation::load($variations_field[0]['target_id']);
+      if (!empty($variation)) {
+        $sku_field = $variation->get('sku')->getValue();
+        if (!empty($sku_field)) {
+          $sku = $sku_field[0]['value'];
+          $modal_form['elements']['stock_number']['#value'] = $sku;
+        }
+      }
+    }
+
+    //model
+    if (!empty($product->hasField('field_car_model'))) {
+      $field_car_model = $product->get('field_car_model')->getValue();
+      if (isset($field_car_model[0]['target_id'])) {
+        $field_car_model_id = $field_car_model[0]['target_id'];
+        $model_term = Term::load($field_car_model_id);
+        $modal_form['elements']['model']['#value'] = $model_term->getName();
+      }
+    }
+
+    //year
+    if (!empty($product->hasField('field_car_year'))) {
+      $field_car_year = $product->get('field_car_year')->getValue();
+      if (!empty($field_car_year)) {
+        $model_year = $field_car_year[0]['value'];
+        $modal_form['elements']['year']['#value'] = date('Y', strtotime($model_year));
+      }
+    }
+
+    //make
+    if (!empty($product->hasField('field_car_make'))) {
+      $field_car_make = $product->get('field_car_make')->getValue();
+      if (!empty($field_car_make)) {
+        $model_make = $field_car_make[0]['value'];
+        $modal_form['elements']['make']['#value'] = $model_make;
+      }
+    }
+
+    // Add an AJAX command to open a modal dialog with the form as the content.
+    $response->addCommand(new OpenDialogCommand('#trilliumlincoln-close-modal', $webform->label(), $modal_form, ['modal' => true]));
+
+    return $response;
+  }
+
 }
